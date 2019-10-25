426da69a632fc9a83a44866b3034555fc3aaf67b
Kayla Reap <knrwdm@mail.missouri.edu>
2019-10-25 18:40:12 -0500

    added color struct and set background color on cells

---
+++ Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  ColorsSol
+//
+//  Created by Kayla Reap on 10/25/19.
+//  Copyright © 2019 Kayla Reap. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
+
